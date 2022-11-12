bfbbdb08bb65bc5848d06d71bd781ddea1a65c5d
mohd-alee <syed.ali_11@hotmail.com>
2022-11-13 04:06:15 +0500

    Task 1 Completed

--- src/App.js
+++ src/App.js
@@ -4,21 +4,49 @@ import './App.css';
 function App() { 
   const name = 'Hello World';
   const obj  = { message: "Welcome to expertizo" };
-  const data = ['We', 'are', 'United']; //Show these in separate tags
+  const data = ['We', 'Are', 'United']; //Show these in separate tags
   const list = [{name: "Hello World 1"}, {name: "Hello World 2"}, {name: "Hello World 3"}]; //Show these in separate tags
-  const complex = [ {company: 'XYZ', jobs: ['JavaScript', 'React']}, {company: 'ABC', jobs: ['Angular', 'Ionic']}]; //Show these in a Table
+  const complex = [
+    {
+      company: 'XYZ',
+      jobs: ['JavaScript', 'React']
+    },
+    {
+      company: 'ABC',
+      jobs: ['Angular', 'Ionic']
+    }
+  ]; //Show these in a Table
   return (
     <div>
       <p><b>Task 1:</b> {name}</p>
       <p><b>Task 2:</b> {obj.message}</p>
       <p><b>Task 3:</b></p>
-      <ul>
-      {
-      data.forEach(element => {
-        <li>{element}</li>;
-      })
-      }
-      </ul>
+      <ol>
+        { data.map((item)=>{ return <li>{item}</li> }) }
+      </ol>
+      <p><b>Task 4:</b></p>
+      <ol>
+        { list.map((item)=>{ return <li>{item.name}</li> }) }
+      </ol>
+      <p><b>Task 5:</b></p>
+      <table>
+        <thead>
+          <th>Company Name</th>
+          <th>Technologies</th>
+        </thead>
+        <tbody>
+          {
+            complex.map((item)=>{
+              return <>
+                <tr>
+                  <td style={{textAlign: "center"}}>{item.company}</td>
+                  <td>{item.jobs.map((item)=>{return item + ' '})}</td>
+                </tr> 
+              </>
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
